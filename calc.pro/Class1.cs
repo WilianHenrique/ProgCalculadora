166d2710d930d79436548ba8a59ea736d263d143
Wilian Henrique <wilian.oliveira@alunos.unis.edu.br>
2020-04-02 13:31:40 -0300

    Meu primeiro commit

---
+++ calc.pro/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calc.pro
+{
+    class Calculadora
+        {
+            private List<double> listNumero = new List<double>();
+            public void addNumero(double num)
+            {
+                this.listNumero.Add(num);
+            }
+            public double somar()
+            {
+                double resultado = 0;
+                foreach (double numero in this.listNumero)
+                {
+                    resultado += numero;
+                }
+                this.listNumero.Clear();
+                return resultado;
+            }
+            public double subtrair()
+            {
+                double resultado = this.listNumero[0];
+                this.listNumero.RemoveAt(0);
+                foreach (double numero in this.listNumero)
+                {
+                    resultado -= numero;
+                }
+                this.listNumero.Clear();
+                return resultado;
+            }
+            public double multiplicar()
+              {
+            double resultado = this.listNumero[0];
+            this.listNumero.RemoveAt(0);
+            foreach (double numero in this.listNumero)
+            {
+                resultado *= numero;
+            }
+            this.listNumero.Clear();
+            return resultado;
+        }
+            public double dividir()
+            {
+               double resultado = this.listNumero[0];
+              this.listNumero.RemoveAt(0);
+               foreach (double numero in this.listNumero)
+               {
+                resultado /= numero;
+               }
+              this.listNumero.Clear();
+              return resultado;
+        }
+        public double porcentagem()
+        {
+            double resultado = this.listNumero[0];
+            this.listNumero.RemoveAt(0);
+            foreach (double numero in this.listNumero)
+            {
+                resultado = (numero * numero) / 100;
+            }
+            this.listNumero.Clear();
+            return resultado;
+        }
+    }
+}
---
+++ calc.pro/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace calc.pro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora c = new Calculadora();
+            c.addNumero(10.1);
+            c.addNumero(2.9);
+
+            System.Console.WriteLine(c.somar());
+
+            c.addNumero(10.1);
+            c.addNumero(4.9);
+
+            System.Console.WriteLine(c.somar());
+
+            c.addNumero(10);
+            c.addNumero(5);
+            c.addNumero(2);
+            System.Console.WriteLine(c.subtrair());
+
+            c.addNumero(10);
+            c.addNumero(2);
+            c.addNumero(2);
+            System.Console.WriteLine(c.multiplicar());
+
+            c.addNumero(10);
+            c.addNumero(2);
+            c.addNumero(2);
+            System.Console.WriteLine(c.dividir());
+
+            System.Console.ReadKey();
+        }
+    }
+}
